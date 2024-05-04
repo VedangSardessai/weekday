ff3dab88e330248d18d6d619b55db265b8374a67
VedangSardessai <vms112000@gmail.com>
2024-05-04 14:25:26 +0530

    ui: added the welcome banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import LeftSideBar from "./left_sidebar/LeftSidebar";
 import RightSideBar from "./right_sidebar/RightSidebar";
+import WelcomeUser from "./welcome_banner/WelcomeUser";
 
 function App() {
   return (
     <div className="App">
       <div className="flex-container">
         <LeftSideBar />
+        <WelcomeUser />
         <div className="content"></div>
         <RightSideBar />
       </div>
