6f9acda2e9ffad780280dbae183997b2616da7b3
VedangSardessai <vms112000@gmail.com>
2024-05-04 19:23:18 +0530

    code: wrapped app with provider

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import "./App.css";
 import LeftSideBar from "./left_sidebar/LeftSidebar";
 import RightSideBar from "./right_sidebar/RightSidebar";
 import WelcomeUser from "./welcome_banner/WelcomeUser";
+import ReduxProvider from "./redux/provider";
 
 function App() {
   return (
-    <div className="App">
-      <div className="flex-container">
-        <LeftSideBar />
-        <WelcomeUser />
-        <div className="content"></div>
-        <RightSideBar />
+    <ReduxProvider>
+      <div className="App">
+        <div className="flex-container">
+          <LeftSideBar />
+          <WelcomeUser />
+          <div className="content"></div>
+          <RightSideBar />
+        </div>
       </div>
-    </div>
+    </ReduxProvider>
   );
 }
 
