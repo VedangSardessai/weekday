32df38527afd2ca0991241345536735b70538737
VedangSardessai <vms112000@gmail.com>
2024-05-04 19:07:02 +0530

    code: created the store and provider

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import roleReducer from "./slices/role-slice";
+import employeeReducer from "./slices/employee-slice";
+import experienceReducer from "./slices/experience-slice";
+import workReducer from "./slices/work-slice";
+import salaryReducer from "./slices/salary-slice";
+import searchReducer from "./slices/search-slice";
+
+export const store = configureStore({
+  reducer: {
+    role: roleReducer,
+    employee: employeeReducer,
+    experience: experienceReducer,
+    work: workReducer,
+    salary: salaryReducer,
+    search: searchReducer,
+  },
+});
