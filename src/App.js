ce9cfdd444173eba2fa8756ee1dbdaef351f3194
VedangSardessai <vms112000@gmail.com>
2024-05-04 14:14:43 +0530

    ui: css for flex-container and content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import LeftSideBar from "./left_sidebar/LeftSidebar";
+import RightSideBar from "./right_sidebar/RightSidebar";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="flex-container">
+        <LeftSideBar />
+        <div className="content"></div>
+        <RightSideBar />
+      </div>
     </div>
   );
 }
